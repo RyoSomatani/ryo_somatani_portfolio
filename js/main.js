78d574ff21f7c42bd67faced5bcfe0dbb60935a3
Ryo Somatani <ryosomatani@Ryos-MacBook-Pro.local>
2019-10-23 21:37:17 +0900

    about page added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-// ---------- Hamburge Menu ----------
-const hamburger = document.querySelector(".hamburger-menu");
-const navMenu = document.querySelector("nav>ul");
+//===========================================
+//==== ハンバーガーメニューアニメーション ===
+//===========================================
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav__list");
 let hamburgerChecked = false;
 
 hamburger.addEventListener("click", hamburgerAnimation);
@@ -30,8 +32,9 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// ------ PROJECT INFO ANIMATION ------
-
+//===========================================
+//==== PROJECT INFO ANIMATIONアニメーション ===
+//===========================================
 const projects = document.querySelectorAll(".hover-overlay");
 
 // hover (mouseenter) イベントでアニメーション開始
@@ -46,4 +49,12 @@ projects.forEach(project => {
     e.preventDefault();
     e.target.children[1].classList.remove("hovered");
   });
+});
+
+//===========================================
+//===== ページタイトル下のバー用アニメーション ====
+//===========================================
+window.addEventListener("load", () => {
+  const pageTitle = document.querySelector(".page-title__title");
+  pageTitle.classList.add("show");
 });
